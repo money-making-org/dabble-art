24b63561012b0b4047d84a2c840c153ed5f127d2
Dawson <hello@dawson.gg>
2025-03-31 19:05:25 -0400

    fix crappy better auth token

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  // const sessionCookie = getSessionCookie(request);
+  const sessionCookie = request.cookies.get("better-auth.session_token");
 
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/auth/sign-in", request.url));
