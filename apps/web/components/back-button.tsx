c179b5acf6f2a45864db48a1d9ce912e63e5613b
Dawson <hello@dawson.gg>
2025-04-14 13:20:20 -0400

    back button works

---
+++ apps/web/components/back-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@workspace/ui/components/button";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export const BackButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant="secondary"
+      asChild
+      className="group"
+      onClick={() => router.back()}
+    >
+      <ArrowLeft
+        className="me-2 ms-0 opacity-60 transition-transform group-hover:-translate-x-0.5"
+        size={16}
+        strokeWidth={2}
+        aria-hidden="true"
+      />
+      Go back
+    </Button>
+  );
+};
