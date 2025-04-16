51c49e0f6e952d11fef78e3cce3c3a59007438e0
simonfoy <simon.g.foy@outlook.com>
2025-04-16 19:18:02 -0400

    Revert "editprofile modal"
    This reverts commit 0c3e3acbcb891e825ab304185ade5a0891263696.

--- apps/web/app/profile/[username]/_components/profile-header.tsx
+++ apps/web/app/profile/[username]/_components/profile-header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { MessageCircle, Palette } from "lucide-react";
-import { useState } from "react";
 
 import {
   Avatar,
@@ -10,16 +9,29 @@ import {
 } from "@workspace/ui/components/avatar";
 import { User } from "better-auth";
 import { ElysiaUser, type UserType } from "@workspace/db/src/schema/users";
-import { Heart, Pencil } from "lucide-react";
+import { Heart } from "lucide-react";
 import { Button } from "@workspace/ui/components/button";
+import { authClient } from "@/lib/auth-client";
+import { FollowButton } from "@/components/follow-button";
 
-export default function ProfileHeader({ user }: { user: UserType }) {
+export default function ProfileHeader({
+  user,
+}: {
+  user: typeof authClient.$Infer.Session.user & {
+    isFollowing: boolean;
+  };
+}) {
+  const { data: session, error } = authClient.useSession();
+
   return (
     <div className="flex flex-col md:flex-row gap-6">
       <div className="flex-shrink-0">
         <div className="relative">
           <Avatar className="size-32 border">
-            <AvatarImage src={user?.image ?? ""} alt={user?.displayUsername} />
+            <AvatarImage
+              src={user?.image ?? ""}
+              alt={user?.displayUsername ?? ""}
+            />
             <AvatarFallback>{user?.username?.[0] || "?"}</AvatarFallback>
           </Avatar>
           <div className="absolute bottom-0 right-0 p-2 bg-primary rounded-full text-primary-foreground">
@@ -34,10 +46,16 @@ export default function ProfileHeader({ user }: { user: UserType }) {
             <p className="text-muted-foreground">@{user?.username}</p>
           </div>
           <div className="flex gap-2">
-            <Button variant="outline" size="sm">
-              <Heart className="w-4 h-4 mr-2" />
-              Follow
-            </Button>
+            {/* Don't show on own profile. Should show if user is not logged in, don't show if session hasn't loaded yet */}
+            {user?.id !== session?.user?.id && (error || session?.user?.id) && (
+              <FollowButton
+                userId={user?.id}
+                info={{
+                  isFollowing: user?.isFollowing,
+                  isFollowPending: false,
+                }}
+              />
+            )}
           </div>
         </div>
         <div className="flex flex-wrap gap-2">
